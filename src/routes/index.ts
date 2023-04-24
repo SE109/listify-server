a751b58998370f46be90999d2d2d940f5807d51c
tuannt02 <20522122@gm.uit.edu.vn>
2023-04-24 20:30:52 +0700

    set up swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express';
 import testRoute from './test.route';
+import apiDocsRoute from './api-docs.route';
 
 const route = (app: Express) => {
+  app.use('/api-docs', apiDocsRoute);
   app.use('/test', testRoute);
 };
 
