7ea4b9128bf04c0b148d7f3e956d69b6d51a9cb0
tuannt02 <20522122@gm.uit.edu.vn>
2023-05-04 23:00:56 +0700

    create grouptask route

--- src/controllers/groupTask.controller.ts
+++ src/controllers/groupTask.controller.ts
@@ -1,55 +1,85 @@
 import { Request, Response, NextFunction } from 'express';
 import createError from 'http-errors';
 import { GroupTask, SubTask, Task, Voice } from '../models';
+import { ResJSON } from '../utils/interface';
 
 export const getAllGroupTaskController = async (
   req: Request,
-  res: Response,
+  res: Response<ResJSON>,
   next: NextFunction
 ) => {
-  // Get userMail from previous middleware
-  const userMail = res.locals.payload.user.mail;
-
-  const groupTaskList = await GroupTask.findAll({
-    where: {
-      userMail,
-    },
-    attributes: {
-      exclude: ['userMail', 'createdAt', 'updatedAt'],
-    },
-    include: [
-      {
-        model: Task,
-        through: {
-          attributes: [],
-        },
-        include: [
-          {
-            model: SubTask,
-            attributes: {
-              exclude: ['createdAt', 'updatedAt'],
-            },
-          },
-          {
-            model: Voice,
-            attributes: {
-              exclude: ['createdAt', 'updatedAt'],
-            },
-          },
-        ],
-        attributes: {
-          exclude: ['userMail', 'createdAt', 'updatedAt'],
-        },
-      },
-    ],
-    nest: true,
-    raw: true,
-  });
-
   try {
+    // Get userMail from previous middleware
+    const userMail = res.locals.payload.user.mail;
+
+    const groupTaskList = await GroupTask.findAll({
+      where: {
+        userMail,
+      },
+      attributes: {
+        exclude: ['userMail', 'createdAt', 'updatedAt'],
+      },
+      include: [
+        {
+          model: Task,
+          through: {
+            attributes: [],
+          },
+          include: [
+            {
+              model: SubTask,
+              attributes: {
+                exclude: ['createdAt', 'updatedAt'],
+              },
+            },
+            {
+              model: Voice,
+              attributes: {
+                exclude: ['createdAt', 'updatedAt'],
+              },
+            },
+          ],
+          attributes: {
+            exclude: ['userMail', 'createdAt', 'updatedAt'],
+          },
+        },
+      ],
+      nest: true,
+      raw: true,
+    });
+
     res.status(200).json({
-      status: 200,
+      statusCode: 200,
+      message: 'Success',
       data: groupTaskList,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const addGroupTaskController = async (
+  req: Request<{}, {}, GroupTask>,
+  res: Response<ResJSON>,
+  next: NextFunction
+) => {
+  try {
+    // Get userMail from previous middleware
+    const userMail = res.locals.payload.user.mail;
+
+    // Todo: Add validation
+    const { name } = req.body;
+
+    // Create new grouptask
+    const createdGroupTask = await GroupTask.create({
+      userMail,
+      name,
+    });
+
+    res.status(200).json({
+      statusCode: 200,
+      message: 'Added successfully',
+      data: createdGroupTask,
     });
   } catch (err) {
     next(err);
--- src/routes/groupTask.route.ts
+++ src/routes/groupTask.route.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import { verifyAccessToken } from '../utils/jwt_service';
-import { getAllGroupTaskController } from '../controllers/groupTask.controller';
+import {
+  getAllGroupTaskController,
+  addGroupTaskController,
+} from '../controllers/groupTask.controller';
 
 const router: Router = Router();
 
 // [GET] /gtask -> Get all GroupTask belong to user
 router.get('/', verifyAccessToken, getAllGroupTaskController);
+
+// [POST] /gtask -> Create new GroupTask
+router.post('/', verifyAccessToken, addGroupTaskController);
 
 export default router;
