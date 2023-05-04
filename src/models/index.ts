ecb02d961f432f0d6e60b88d949d43f8df69c6ca
tuannt02 <20522122@gm.uit.edu.vn>
2023-05-04 15:27:45 +0700

    add SubTask model

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,5 +3,6 @@ import User from './user.model';
 import TaskList from './taskList.model';
 import Task from './task.model';
 import TaskIncluded from './taskIncluded.model';
+import SubTask from './subTask.model';
 
-export { Test, User, TaskList, Task, TaskIncluded };
+export { Test, User, TaskList, Task, TaskIncluded, SubTask };
---
+++ src/models/subTask.model.ts
@@ -0,0 +1,42 @@
+import {
+  Table,
+  Column,
+  Model,
+  CreatedAt,
+  UpdatedAt,
+  DataType,
+  BelongsTo,
+  ForeignKey,
+} from 'sequelize-typescript';
+import { Task } from '../models';
+
+@Table({ modelName: 'SubTask', tableName: 'sub_task' })
+class SubTask extends Model {
+  // Associations
+  @BelongsTo(() => Task)
+  task!: Task;
+
+  // Columns
+  @Column({ type: DataType.INTEGER, primaryKey: true, autoIncrement: true, field: 'sub_task_id' })
+  id!: number;
+
+  @ForeignKey(() => Task)
+  @Column({ type: DataType.INTEGER, field: 'task_id' })
+  taskId!: string;
+
+  @Column({ type: DataType.STRING, field: 'title' })
+  title!: string;
+
+  @Column({ type: DataType.BOOLEAN, field: 'is_completed' })
+  isCompleted!: boolean;
+
+  @CreatedAt
+  @Column({ type: DataType.DATE, field: 'created_at' })
+  createdAt?: Date;
+
+  @UpdatedAt
+  @Column({ type: DataType.DATE, field: 'updated_at' })
+  updatedAt?: Date;
+}
+
+export default SubTask;
