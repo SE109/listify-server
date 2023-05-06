237cc57f2e6eab930fe7f79a363228e1110547da
tuannt02 <20522122@gm.uit.edu.vn>
2023-05-06 08:18:03 +0700

    get single voice by id route

--- src/controllers/voice.controller.ts
+++ src/controllers/voice.controller.ts
@@ -46,3 +46,48 @@ export const getAllVoiceOfTaskController = async (
     next(err);
   }
 };
+
+export const getSingleVoiceByIdController = async (
+  req: Request<{ voiceId: string }>,
+  res: Response<ResJSON, { payload: IPayload }>,
+  next: NextFunction
+) => {
+  try {
+    // Get userMail from previous middleware
+    const userMail = res.locals.payload.user.mail;
+
+    const { voiceId: unconvertVoiceId } = req.params;
+    const voiceId: number = +unconvertVoiceId;
+
+    const task = await Task.findOne({
+      where: {
+        userMail,
+      },
+      include: {
+        model: Voice,
+        where: {
+          id: voiceId,
+        },
+        attributes: {
+          exclude: ['taskId', 'updatedAt', 'createdAt'],
+        },
+      },
+      raw: true,
+      nest: true,
+    });
+
+    if (!task) {
+      throw createError.BadRequest('voiceId does not exist');
+    }
+
+    const voice = task.voiceList;
+
+    res.status(200).json({
+      statusCode: 200,
+      message: 'Success',
+      data: voice,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
--- src/routes/voice.route.ts
+++ src/routes/voice.route.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { getAllVoiceOfTaskController } from '../controllers/voice.controller';
+import {
+  getAllVoiceOfTaskController,
+  getSingleVoiceByIdController,
+} from '../controllers/voice.controller';
 import { verifyAccessToken } from '../utils/jwt_service';
 
 const router: Router = Router();
 
 // [GET] /voice/vtask/{taskId} -> Get all voice of task
 router.get('/vtask/:taskId', verifyAccessToken, getAllVoiceOfTaskController);
+
+// [GET] /voice/{voiceId} -> Get single voice by id
+router.get('/:voiceId', verifyAccessToken, getSingleVoiceByIdController);
 
 export default router;
